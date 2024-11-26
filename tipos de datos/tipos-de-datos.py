55efe686c90987fcd871cd64d818844a4241747f
adrian <adrianvigar17@gmail.com>
2024-11-26 13:22:39 +0100

    Python

---
+++ tipos de datos/tipos-de-datos.py
@@ -0,0 +1,17 @@
+print("string")
+
+a = 2
+
+nombre = 5
+bienv = f"Hola {nombre} como estas"
+
+print("Hola" in bienv)
+
+#Comentarios en este código
+
+
+lista = ["Lucas", 5 , True]
+conjunto = {"Adrian" , "Soy" , False}
+
+print(lista[0])
+
